8db9913dc439822f2a8c318b2b5b66c7a4b3a8be
Adi <adi@Adis-MacBook-Air.local>
2022-04-28 17:46:56 +0300

    Addition of main

--- main.c
+++ main.c
@@ -1,121 +1,33 @@
+#include <stdio.h>
 #include "RLEList.h"
 #include "RLEList.h"
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
-}
 
+int main(int argc, char** argv) {
+    if (argc != 3) {
+        printf("Problem: copy <file1> <file2>", NULL);
+        return 0;
+    }
+    FILE *input = fopen(argv[1], "r");
+    if (!input) {
+        printf("Error: cannot open", argv[1]);
+        return 0;
+    }
+    FILE *output = fopen(argv[2], "w");
+    if (!output) {
+        fclose(input);
+        printf("Error: cannot open", argv[2]);
+        return 0;
+    }
 
-struct RLEList_t
-{
-    char letter;
-    int times;
-    struct RLEList_t* next;
-};
-
-RLEList RLEListCreate()
-{
-    RLEList list=malloc(sizeof(*list));
-    if (list==NULL)
+    if (argv[0] == '-e')
     {
-        return NULL;
+        
     }
-    *list.letter = "\0";
-    *list.times = 0;
-    *list.next = NULL;
-    return list;
-}
 
-void RLEListDestroy(RLEList list) {
-    while(list) {
-        RLEList toDelete = list;
-        list = list->next;
-        free(toDelete);
-    }
-}
-
-RLEListResult RLEListAppend (RLEList list, char value)
-{
-    if ((*list.letter == '\0') || (list.times == NULL)||(list == NULL)||(value=='\0'))
+    if (argv[0] == '-i')
     {
-        return RLE_LIST_NULL_ARGUMENT;
+
     }
-    if (value == *list.letter)
-    {
-        *list.times+=1;
-    }
-    else
-    {
-        *list.next= RLEListCreate();
-        if (*list.next == NULL)
-        {
-            return RLE_LIST_OUT_OF_MEMORY;
-        }
-        **list.next.letter = value;
-        **list.next.times = 1;
-        return RLE_LIST_SUCCESS;
-    }
+
 }
-
-
-int RLEListSize(RLEList list)
-{
-    int count = 0;
-    while (list){
-        count += *list.times;
-        list = *list.next;
-    }
-    return count;
-}
-
-char RLEListGet(RLEList list, int index, RLEListResult *result)
-{
-    *result = RLEListOfIndex(list, index);
-    if (result == RLE_LIST_SUCCESS)
-    {
-        return *list.letter;)
-    }
-    return 0;
-}
-
-RLEListResult RLEListRemove(RLEList list, int index)
-{
-    RLEListResult currentResult = RLEListOfIndex(list, index);
-    if (*list.times == 1){
-        *list.next = **list.next.next;
-        *list.times = **list.next.times;
-        *list.letter = **list.next.letter;
-        list = *list.next;
-        free(list);
-    }
-    else {
-        *list.times -= 1;
-    }
-}
-
-
-RLEListResult RLEListOfIndex(RLEList list, int index)
-{
-    if ((*list.letter == '\0') || (*list.times == NULL)||(list == NULL))
-    {
-        return RLE_LIST_NULL_ARGUMENT;
-    }
-    int temp = *list.times;
-    while (index){
-        index -= 1;
-        if (index == 0){
-            return RLE_LIST_SUCCESS;
-        }
-        temp -= 1;
-        if (temp == 0) {
-            if (*list.next == NULL) {
-                return RLE_LIST_INDEX_OUT_OF_BOUNDS;
-            }
-            list = *list.next;
-            temp = *list.times;
-        }
-    }
-}
-
-//implement the functions here
