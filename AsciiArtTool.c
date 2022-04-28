438af36f7947a48d0680f69821cf3dda6f3890eb
bar kedem <barkedem19@gmail.com>
2022-04-28 15:04:20 +0300

    RLEList syntax correction, AsciiArt added functions

--- AsciiArtTool.c
+++ AsciiArtTool.c
@@ -3,28 +3,62 @@
 
 #define NODE_INFO 3
 #define EMPTY_POINTER -1
+#define BUFFER_SIZE 1
 
 // כותבים כל מה שצריך בתוך התוכנית יש להם ifndef בפנים כנגיע ללינקר
+//check how to access members of struct RLEList
 
 RLEList asciiArtRead(FILE* in_stream)
 {
-    open??
-    char* fgets(char* buffer, int size, FILE* stream)
+    RLEList header = RLEListCreate();
+    RLEList ptr = header;
+    char buffer [BUFFER_SIZE] = "";
+    //don't need open, we got the file pointer, probably opened in the calling function.
+    fgets(buffer,BUFFER_SIZE,in_stream);//check if buffer size should be +1 of buffer to accommodate \0;
+    //check input error (returns NULL)
+    header->letter = buffer[0];
+    //check if empty
+    RLEListAppend(ptr,buffer[0])
+    //finish implementation after fixing append (need to decide what happens to list)
+    //loop until reaches NULL, return header (not ptr)
 }
 
+//prints given list to file (not encoded)
 RLEListResult asciiArtPrint(RLEList list, FILE *out_stream)
 {
-    int aAa = fputs(const char* str, FILE* stream);
-    if (aAa =!)
-        return fasfs;
+    RLEList ptr = list; // pointer so header won't change
+    if ((list == NULL)||(out_stream==NULL))
+    {
+        return RLE_LIST_NULL_ARGUMENT;
+    }
+    while (ptr != NULL)
+    {
+        char toPrint = ptr->letter;
+        for (int counter = ptr->times; counter > 0; counter--) // number of times to print char
+        {
+            if (fputs(&toPrint, out_stream) == EOF)
+            {
+                return RLE_LIST_ERROR;
+            }
+        }
+        ptr = ptr->next;
+    }
+    return RLE_LIST_SUCCESS;
 }
-
 
 RLEListResult asciiArtPrintEncoded(RLEList list, FILE *out_stream)
 {
-    int aAa = fputs(RLEListExportToString(RLEList list, RLEListResult* result), FILE* stream);
-
+    RLEListResult result;
+    if ((list == NULL)||(out_stream==NULL))
+    {
+        return RLE_LIST_NULL_ARGUMENT;
+    }
+    if (fputs(RLEListExportToString(list,&result),out_stream) == NULL)
+    {
+        return RLE_LIST_ERROR;
+    }
+    else
+    {
+        return RLE_LIST_SUCCESS;
+    }
 }
-
-
-RLEListExportToString(RLEList list, RLEListResult* result)
